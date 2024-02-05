00077e7743aca425a1962145091fe73cde7650a5
tarun268 <tarun@prodios.com>
2024-02-05 15:30:20 +0530

    feat(register): add register functionality using server actions

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,18 +1,36 @@
 'use server'
 
 import * as z from 'zod'
-import { LoginSchema, RegisterSchema } from '@/schemas'
+import bcrypt from 'bcrypt'
+import { RegisterSchema } from '@/schemas'
+import { prisma } from '@/lib/db'
+import { getUserByEmail } from '@/data/user'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-  const validatedFields = LoginSchema.safeParse(values)
+  const validatedFields = RegisterSchema.safeParse(values)
 
   if (!validatedFields.success) {
-    return {
-      error: 'Invalid fields',
-    }
+    return { error: 'Invalid fields!' }
   }
 
+  const { email, password, name } = validatedFields.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    return { error: 'Email already in use!' }
+  }
+
+  await prisma.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  })
+
   return {
-    success: 'Email sent!',
+    success: 'User created!',
   }
 }
